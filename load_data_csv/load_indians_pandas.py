2276a6efbd8c2a999acd28ad63d56faca3cf9d5d
Thanh Nguyen <nguyenphuongthanhf@gmail.com>
2019-07-05 17:34:28 +0700

    updaet

--- load_data_csv/load_indians_pandas.py
+++ load_data_csv/load_indians_pandas.py
@@ -1,11 +1,13 @@
 from pandas import read_csv
 from pandas import set_option
 from matplotlib import pyplot
-
+import numpy
+from pandas.plotting import scatter_matrix
 
 #  1. LOAD DATA
 filename = 'pima-indians-diabetes.data.csv'
-names=['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+names = ['preg', 'plas', 'pres', 'skin',
+         'test', 'mass', 'pedi', 'age', 'class']
 # Load data from link
 #url = ' https://goo.gl/vhm1eU '
 #data = read_csv(url, names=names)
@@ -61,9 +63,9 @@ print('==============CORRELATIONS BETWEEN ATTRIBUTES==============')
 print('*')
 
 correlations = data.corr(method='pearson')
-print(correlations);
+print(correlations)
 
-# 2.7 Skew of Univariate Distributions. Độ nghiêng của mô hình phân bố 
+# 2.7 Skew of Univariate Distributions. Độ nghiêng của mô hình phân bố
 print('*')
 print('===============Skew of Univariate Distributions==============')
 print('*')
@@ -71,10 +73,119 @@ print('*')
 skew = data.skew()
 print(skew)
 
-# 3 Understand Your Data With Visualization 
+# 3 Understand Your Data With Visualization
 
-# 3.1 
+# 3.1 Univariate Plots
 # 3.1.1 Histograms
+print('*')
+print('=============== Histograms ==============')
+print('*')
+
 data.hist()
-#pyplot.show()
-pyplot.savefig("matplotlib.png")
+# pyplot.show()
+pyplot.savefig("histograms.png")
+
+# 3.1.1 Density Plots
+print('*')
+print('=============== Density Plots ==============')
+print('*')
+
+data.plot(kind='density', subplots=True, layout=(3, 3), sharex=False)
+# pyplot.show()
+pyplot.savefig("density.png")
+
+
+# 3.1.2 Box and Whisker Plots
+print('*')
+print('=============== Box and Whisker Plots ==============')
+print('*')
+
+data.plot(kind='box', subplots=True, layout=(3, 3), sharex=False, sharey=False)
+# pyplot.show()
+pyplot.savefig("box_whisker.png")
+
+# 3.2 Multivariate Plots
+# 3.2.1 Correlation Matrix Plot
+print('*')
+print('=============== Correlation Matrix Plot ==============')
+print('*')
+
+correlations = data.corr()
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(correlations, vmin=-1, vmax=1)
+fig.colorbar(cax)
+ticks = numpy.arange(0, 9, 1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
+
+# pyplot.show()
+pyplot.savefig("Correlation_Matrix_Plot.png")
+
+# 3.2.2  Scatter Plot Matrix
+print('*')
+print('===============  Scatter Plot Matrix ==============')
+print('*')
+
+scatter_matrix(data)
+# pyplot.show()
+pyplot.savefig("Scatter_Plot_Matrix.png")
+
+
+# 4.1 Prepare Your Data For MachineLearning
+print('*')
+print('===============  Prepare Your Data For MachineLearning ==============')
+print('*')
+
+
+# 4.1.1 Rescale Data
+print('*')
+print('===============  Rescale Data ==============')
+print('*')
+from sklearn.preprocessing import MinMaxScaler
+from numpy import set_printoptions
+
+array = data.values
+#separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+scaler = MinMaxScaler(feature_range=(0,1))
+rescaledX = scaler.fit_transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+
+print(rescaledX[0:5,:])
+
+# 4.1.1 Standardize Data
+print('*')
+print('===============  Standardize Data ==============')
+print('*')
+
+from sklearn.preprocessing import StandardScaler
+
+standard_scaler = StandardScaler().fit(X)
+rescaledX = scaler.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(rescaledX[0:5,:])
+
+print(type(rescaledX))
+
+# 4.1.1 Normalize Data
+print('*')
+print('===============  Normalize Data ==============')
+print('*')
+
+from sklearn.preprocessing import Normalize
+
+scaler= Normalize().fit(X)
+normalizedX = scaler.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(normalizedX[0:5,:])
+
+
+
+
